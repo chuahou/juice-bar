a1ee817614fe343c52ce2b7f1a1d3a34d977c949
Chua Hou <human+github@chuahou.dev>
2022-11-05 20:22:21 +0000

    BarView: draw sub-bars
    - Everything's hardcoded for now for personal use.

--- app/src/main/java/dev/chuahou/juicebar/BarView.java
+++ app/src/main/java/dev/chuahou/juicebar/BarView.java
@@ -11,7 +11,32 @@ import android.view.View;
 public class BarView extends View {
     private float battLevel = 0.0f;
 
-    private ShapeDrawable drawable;
+    /** Single sub-bar, representing one level. */
+    private class SubBar {
+        private ShapeDrawable rect;
+        private float maxPercentage; // % of battery corresponding to this sub-bar.
+        private static final int height = 8; // Height of sub-bar.
+        public SubBar(float maxPercentage, int color) {
+            rect = new ShapeDrawable(new RectShape());
+            rect.getPaint().setColor(color);
+            this.maxPercentage = maxPercentage;
+        }
+        public void draw(Canvas canvas) {
+            float percentage = Math.min(battLevel, maxPercentage);
+            Log.i(TAG, "Drawing sub-bar with max at " + maxPercentage + "% at " + percentage + "%");
+            rect.setBounds(0, 0, (int) (canvas.getWidth() * percentage), height);
+            rect.draw(canvas);
+        }
+    }
+
+    // Drawn from first element to last. The later in the array, the higher the sub-bar.
+    private final SubBar subBars[] = {
+        new SubBar(1.0000f, 0xFF0099CC), // 80%
+        new SubBar(0.8125f, 0xFFC1E859), // 65%
+        new SubBar(0.4375f, 0xFFF0A30A), // 35%
+        new SubBar(0.1875f, 0xFFE74C3C), // 15%
+        new SubBar(0.0625f, 0xFFFF2D3B), //  5%
+    };
 
     /** Tag for logging. */
     private static final String TAG = "juicebar.BarView";
@@ -28,17 +53,13 @@ public class BarView extends View {
 
     public BarView(Context context) {
         super(context);
-        drawable = new ShapeDrawable(new RectShape());
-        drawable.getPaint().setColor(0xff00ff00);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         Log.i(TAG, "Redrawing");
         super.onDraw(canvas);
-        int width = (int) (canvas.getWidth() * battLevel);
-        int height = 8;
-        drawable.setBounds(0, 0, width, height);
-        drawable.draw(canvas);
+        for (SubBar subBar : subBars)
+            subBar.draw(canvas);
     }
 }
